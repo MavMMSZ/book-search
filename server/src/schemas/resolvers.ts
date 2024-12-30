6bcadee7c0262348cf83b699a2143b4a4c01d68f
Maverick S <maverickmmsc@gmail.com>
2024-12-30 18:08:08 -0500

    added typeDefs

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,31 @@
+import Book, { IBook } from '../models/book';
+import User, { IUser } from '../models/user';
+
+const resolvers = {
+    Query: {
+        book: async (): Promise<IBook[] | null> => {
+            return Book.find({});
+        },
+        user: async (_parent: any, { _id }: { _id: string }): Promise<IUser[] | null> => {
+            const params = _id ? { _id } : {};
+            return User.find(params);
+        },
+    },
+    Mutation: {
+        createBook: async (_parent: any, args: any): Promise<IBook | null> => {
+            const book = await Book.create(args);
+            return book;
+        },
+        createUser: async (_parent: any, args: any): Promise<IUser | null> => {
+            const user = await User.create(args);
+            return user;
+        },
+        saveBook: async (_parent: any, { _id, bookId }: { _id: string, bookId: string }): Promise<IUser | null> => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id },
+                { $addToSet: { savedBooks: bookId } },
+                { new: true }
+            );
+            return updatedUser;
+        },
+    },
